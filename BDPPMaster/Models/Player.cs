60c53fe87b4549997c14d36a6cd19f1b13e14518
Amy Khosa <amkho@developer508.builddirect.com>
2015-07-09 14:06:26 -0700

    players model

---
+++ BDPPMaster/Models/Player.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BDPPMaster.Models
+{
+    public class Player
+    {
+        public int firstName { get; set; }
+        public int lastName { get; set; }
+    }
+}
