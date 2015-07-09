0b85446b08af87f750a892d08dfd66943dd86e02
Bessie Chan <bessiechan@builddirect.com>
2015-07-09 16:15:17 -0700

    front end for scoreboard

--- BDPPMaster/App_Start/BundleConfig.cs
+++ BDPPMaster/App_Start/BundleConfig.cs
@@ -23,6 +23,9 @@ namespace BDPPMaster
                       "~/Scripts/bootstrap.js",
                       "~/Scripts/respond.js"));
 
+            bundles.Add(new ScriptBundle("~/bundles/bdpp").Include(
+                      "~/Scripts/bdpp.js"));
+
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
